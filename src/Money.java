dc505b9d981adf61ef958330276924054c53d1a6
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-05-19 12:52:44 -0300

    FINAL COMMIT & PUSH.

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,4 @@
-abstract class Money  {
+class Money  {
   protected int amount;
   protected String currency;
 
@@ -16,11 +16,13 @@ abstract class Money  {
     this.currency = currency;
   }
 
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
 
   public boolean equals(Object object) {
     Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
+    return amount == money.amount && currency().equals(money.currency());
   }
 
   String currency() {
